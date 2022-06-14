b5bb72c25003000dab12cfba8211acc8018b2dd1
gandisrt0107 <gandisratnac@upi.edu>
2022-06-15 02:16:03 +0700

    update app
    okey

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from flask import Flask, request, jsonify, render_template
 
 from fnd import prediction
 
+import os
+import app 
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -19,4 +22,5 @@ def predict():
     return render_template("index.html", predictions = predictions, news = news)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
